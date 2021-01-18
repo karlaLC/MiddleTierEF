87e1bfb11328a78d0afee77b3e82acff5ef05c25
karlaLC <35314415+karlaLC@users.noreply.github.com>
2021-01-18 13:43:23 -0800

    Create ProjectBLL.cs

---
+++ SampleSelectionModal/ProjectBLL.cs
@@ -0,0 +1,34 @@
+        /// <summary>
+        /// Gets projects and product listed locations corresponding to clients in array and updates its sample selection note
+        /// Updates the standard's note as well 
+        /// </summary>
+        /// <param name="ssNoteDto">Dto holding sample selection note, standard id and array of clients' ids</param>
+        public void UpdateSampleSelectionNoteByClientIds(ProjectSampleSelectionNoteDto ssNoteDto)
+        {
+            string newStandardSampleSelectionNote = ssNoteDto.SampleSelectionNote;
+
+            Standard existingStandard = UnitOfWork.Standards.GetQueryable().First(x => x.Id == ssNoteDto.StandardId);
+
+            existingStandard.SampleSelectionNote = newStandardSampleSelectionNote;
+
+            foreach (int id in ssNoteDto.ClientIds)
+            {
+                //get list of projects for the client where the standardId matches the StandardId in the ssNoteDto
+                List<Project> projects = GetQueryable().Where(x => x.ClientId == id &&
+                                                                   (x.ProductCode.Standards.Any(y => y.StandardId == ssNoteDto.StandardId) ||
+                                                                    x.ListedProducts.Any(z => z.AdditionalRecognitions.Any(a => a.AdditionalRecognition.StandardId == ssNoteDto.StandardId)))).ToList();
+
+                foreach (Project project in projects)
+                {
+                    project.SampleSelectionNote = newStandardSampleSelectionNote;
+
+                    //get productListedLocations for project, set SampleSelectionNote
+                    List<ProductListedLocation> productListedLocationsForProject = UnitOfWork.ProductListedLocations.GetQueryable().Where(x => x.ProjectId == project.Id).ToList();
+
+                    foreach (ProductListedLocation location in productListedLocationsForProject)
+                    {
+                        location.SampleSelectionNote = newStandardSampleSelectionNote;
+                    }
+                }
+            }
+        }
